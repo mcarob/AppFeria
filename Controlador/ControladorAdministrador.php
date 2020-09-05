d5d93cdf54785bbd93968133f6c46d4f72e52d82
PipeZambrano21 <70823126+PipeZambrano21@users.noreply.github.com>
2020-09-05 17:50:59 -0500

    Create ControladorAdministrador.php

---
+++ Controlador/ControladorAdministrador.php
@@ -0,0 +1,53 @@
+<?php
+
+include_once($_SERVER['DOCUMENT_ROOT'].'/Proyecto_Feria/AppFeria/Modelo/Daos/AdministradorDAO.php');
+
+
+
+class ControladorAdministrador{
+
+private $administrador;
+
+/*
+public function darInformacion($pCodigo)
+{
+	$this->promociones=new PromocionLaboralDAO();
+	return $this->promociones->detallesPostulacion($pCodigo);
+}
+
+public function darVacantase($pCodigo)
+{
+	$empresa_DAO=new EmpresaDAO();
+	$codigo=$empresa_DAO->darEmpresaXCodigo($pCodigo)->getCodEmpresa();
+	$this->promociones = new PromocionLaboralDAO();
+	return $this->promociones->verOfertas($codigo);
+}
+*/
+
+
+public function actualizarPromocion($pPromocion)
+{
+	$this->promociones=new PromocionLaboralDAO();
+	return $this->promociones->editarVacante($pPromocion);
+}
+
+public function darAdministrador($pCodigo)
+{
+	$administrador_DAO=new AdministradorDAO();
+	$administrador=$administrador_DAO->darAdministradorXCodigo($pCodigo);
+	return $administrador;
+}
+
+public function actualizarAdministrador(Administrador $administrador)
+{
+	$administrador_DAO=new AdministradorDAO();
+	$resultado_administrador=$administrador_DAO->editarPerfil($administrador);
+	return $resultado_administrador;
+}
+
+}
+
+
+
+
+?>
