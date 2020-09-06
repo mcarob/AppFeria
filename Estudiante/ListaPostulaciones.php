df8b578c9ea4520edf6dfcbc0793d4260183eb1b
acubilloso <acubilloso@unbosque.edu.co>
2020-09-06 02:46:39 -0500

    Lista postulaciones

--- Estudiante/ListaPostulaciones.php
+++ Estudiante/ListaPostulaciones.php
@@ -3,34 +3,47 @@ session_start();
 if (!isset($_SESSION['user'])) {
 
     header("location: ../index.php");
-} else if (!$_SESSION['tipo'] == 2 ) {
+} else if (!$_SESSION['tipo'] == 2) {
     header("location: ../index.php");
 }
 
+
+include_once($_SERVER['DOCUMENT_ROOT'] . '/ProyectoFeria/AppFeria/Controlador/ControladorPostulaciones.php');
+$controlador = new ControladorPostulacion();
+$lista = $controlador->darListaPostulacionesxEst(1);
+
+
 ?>
 
+
+<link href="../assets/plugins/nprogress/nprogress.css" rel="stylesheet" />
+<link href="../assets/plugins/daterangepicker/daterangepicker.css" rel="stylesheet" />
+<link href="../assets/plugins/data-tables/datatables.bootstrap4.min.css" rel="stylesheet" />
+<link href="../assets/plugins/data-tables/responsive.datatables.min.css" rel="stylesheet" />
+<script src="../jsPDF/dist/jspdf.es.min.js"></script>
+<script src="../autoTable/dist/jspdf.plugin.autotable.min.js"></script>
 <?php
 include('Header.php');
-include('menuEstudiante.php');
-
+include('menuEstudiante.php')
 ?>
+
 
 <div class="content-wrapper">
     <div class="content">
         <div class="breadcrumb-wrapper">
-            <h1>Vacantes</h1>
+            <h1>Postulaciones hechas</h1>
 
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb p-0">
                     <li class="breadcrumb-item">
-                        <a href="IndexAdmi.php">
+                        <a href="index.php">
                             <span class="mdi mdi-home"></span>
                         </a>
                     </li>
                     <li class="breadcrumb-item">
-                        Mis postulaciones
+                        Vacantes
                     </li>
-                    <li class="breadcrumb-item" aria-current="page">Listar</li>
+                    <li class="breadcrumb-item" aria-current="page">Mis Postulaciones</li>
                 </ol>
             </nav>
 
@@ -39,6 +52,11 @@ include('menuEstudiante.php');
         <div class="row">
             <div class="col-12">
                 <div class="card card-default">
+                    <div class="col-md-4">
+
+                        <br>
+                    </div>
+
 
 
                     <div class="card-body">
@@ -46,32 +64,41 @@ include('menuEstudiante.php');
                             <table id="responsive-data-table" class="table dt-responsive nowrap" style="width:100%">
                                 <thead>
                                     <tr>
-                                        <th>Nombre</th>
-                                        <th>Titulo</th>
-                                        <th>Dirección</th>
-                                        <th>Celular</th>
-                                        <th>Telefono</th>
-                                        <th>Salario</th>
-                                        <th>Correo</th>
+                                        <th>Nombre empresa</th>
+                                        <th>Titulo Oferta</th>
+                                        <th>Telefono empresa</th>
+                                        <th>Telefono contacto</th>
+                                        <th>Correo contacto</th>
+                                        <th>Compensación</th>
                                         <th>Estado</th>
+
 
                                     </tr>
                                 </thead>
 
                                 <tbody>
-                                    <tr>
-                                        <td>Ecopetrol</td>
-                                        <td>Ingeniero de sistemas</td>
-                                        <td>Calle 134</td>
-                                        <td>3214571224</td>
-                                        <td>4021402</td>
-                                        <td>1'200.000</td>
-                                        <td>ecopetrol@gmail.com</td>
-                                        <td><span class="badge badge-primary">Hoja enviada</span></td>
+                                    <?php
+                                    foreach ($lista as $key) {
+                                        echo ("<tr>");
+                                        echo ("<td>" . $key[0] . "</td>");
+                                        echo ("<td>" . $key[1] . "</td>");
+                                        echo ("<td>" . $key[3] . "</td>");
+                                        echo ("<td>" . $key[5] . "</td>");
+                                        echo ("<td>" . $key[4] . "</td>");
+                                        echo ("<td>" . $key[2] . "</td>");
+                                        echo ("<td><span class='mb-2 mr-2 badge badge-success'>" . $key[7] . "</span></td>");
 
-                                    </tr>
 
-                                    
+                                    ?>
+
+
+                                    <?php
+
+                                        echo ("</tr>");
+                                    }
+
+                                    ?>
+
 
                                 </tbody>
                             </table>
@@ -80,40 +107,39 @@ include('menuEstudiante.php');
                 </div>
             </div>
         </div>
+    </div>
 
 
 
 
-        <script src="../assets/plugins/jquery/jquery.min.js"></script>
-        <script src="../assets/plugins/slimscrollbar/jquery.slimscroll.min.js"></script>
-        <script src="../assets/plugins/jekyll-search.min.js"></script>
+    <script src="../assets/plugins/data-tables/jquery.datatables.min.js"></script>
+    <script src="../assets/plugins/data-tables/datatables.bootstrap4.min.js"></script>
 
 
 
-        <script src="../assets/plugins/daterangepicker/moment.min.js"></script>
-        <script src="../assets/plugins/daterangepicker/daterangepicker.js"></script>
-
-
-
-
-        <script src="../assets/plugins/data-tables/jquery.datatables.min.js"></script>
-        <script src="../assets/plugins/data-tables/datatables.bootstrap4.min.js"></script>
-
-
-
-        <script src="../assets/plugins/data-tables/datatables.responsive.min.js"></script>
-        <script>
-            jQuery(document).ready(function() {
-                jQuery('#responsive-data-table').DataTable({
-                    "aLengthMenu": [
-                        [20, 30, 50, 75, -1],
-                        [20, 30, 50, 75, "All"]
-                    ],
-                    "pageLength": 20,
-                    "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
-                });
+    <script src="../assets/plugins/data-tables/datatables.responsive.min.js"></script>
+    <script>
+        jQuery(document).ready(function() {
+            jQuery('#responsive-data-table').DataTable({
+                "aLengthMenu": [
+                    [20, 30, 50, 75, -1],
+                    [20, 30, 50, 75, "All"]
+                ],
+                "pageLength": 20,
+                "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
             });
-        </script>
-        <?php
-        include('Footer.php');
-        ?>
+        });
+    </script>
+
+
+
+
+
+
+
+    <?php
+
+
+    include('Footer.php')
+
+    ?>
