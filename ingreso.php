98e27f6502ce1e4d1ef41df5ae00542211c3c18c
mcarob <37860253+mcarob@users.noreply.github.com>
2020-09-09 17:22:31 -0500

    Update ingreso.php

--- ingreso.php
+++ ingreso.php
@@ -50,6 +50,8 @@ body {
 }
 
     </style>
+    <br><br><br><br><br><br><br><br><br>
+    
       <div class="container d-flex flex-column justify-content-between vh-100">
       <div class="row justify-content-center mt-5">
         <div class="col-xl-5 col-lg-6 col-md-10">
