df9f6f7359f2eaadb1a8042aee047751807c8a30
acubilloso <acubilloso@unbosque.edu.co>
2020-09-10 02:08:28 -0500

    Update BuscarCandidatos.php

--- Empresa/BuscarCandidatos.php
+++ Empresa/BuscarCandidatos.php
@@ -9,6 +9,22 @@ if (!isset($_SESSION['user'])) {
 
 include_once($_SERVER['DOCUMENT_ROOT'] . '/ProyectoFeria/AppFeria/Modelo/Entidades/Estudiante.php');
 include_once($_SERVER['DOCUMENT_ROOT'] . '/ProyectoFeria/AppFeria/Controlador/ControladorEstudiantes.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/ProyectoFeria/AppFeria/Controlador/user.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/ProyectoFeria/AppFeria/Modelo/Daos/EmpresaDAO.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/ProyectoFeria/AppFeria/Modelo/Entidades/Empresa.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/ProyectoFeria/AppFeria/Controlador/ControladorPromocion.php');
+
+
+
+$user = new Usuario();
+$empresa_dao = new EmpresaDAO();
+$user->setUser($_SESSION['user']);
+$codigo= $user->darCodigo();
+$empresa = $empresa_dao->devolverEmpresa($codigo);
+$controlador = new ControladorPromocion();
+
+
+$for = $controlador->verOfertas2($empresa->getCodEmpresa());
 
 $objeto = new ControladorEstudiantes();
 $lista = $objeto->darListaEstudianteActivos();
@@ -22,8 +38,8 @@ $lista = $objeto->darListaEstudianteActivos();
 
 
 <?php
-   include('Header.php');
-   include('menuEmpresa.php');
+include('Header.php');
+include('menuEmpresa.php');
 ?>
 
 
@@ -78,9 +94,9 @@ $lista = $objeto->darListaEstudianteActivos();
                                         echo ("<td>" . $key[5] . "</td>");
                                         echo ("<td>" . $key[6] . "</td>");
                                         echo ("<td>" . $key[7] . "</td>");
-                                        echo ("<td><button type='button' class='mb-1 btn btn-success'>Contactar</button></td>");
+                                        echo ("<td><button type='submit' class='mb-1 btn btn-danger' id='boton1'"."onclick='mostrarModal1(".'"'.$key[0].'"'.")'". ">Contactar</button></td>");
 
-                                        
+
                                     ?>
 
 
@@ -107,24 +123,24 @@ $lista = $objeto->darListaEstudianteActivos();
 
 
 
-    <script src="../assets/plugins/data-tables/jquery.datatables.min.js"></script>
-    <script src="../assets/plugins/data-tables/datatables.bootstrap4.min.js"></script>
+<script src="../assets/plugins/data-tables/jquery.datatables.min.js"></script>
+<script src="../assets/plugins/data-tables/datatables.bootstrap4.min.js"></script>
 
 
 
-    <script src="../assets/plugins/data-tables/datatables.responsive.min.js"></script>
-    <script>
-        jQuery(document).ready(function() {
-            jQuery('#responsive-data-table').DataTable({
-                "aLengthMenu": [
-                    [20, 30, 50, 75, -1],
-                    [20, 30, 50, 75, "All"]
-                ],
-                "pageLength": 20,
-                "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
-            });
+<script src="../assets/plugins/data-tables/datatables.responsive.min.js"></script>
+<script>
+    jQuery(document).ready(function() {
+        jQuery('#responsive-data-table').DataTable({
+            "aLengthMenu": [
+                [20, 30, 50, 75, -1],
+                [20, 30, 50, 75, "All"]
+            ],
+            "pageLength": 20,
+            "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
         });
-    </script>
+    });
+</script>
 
 
 
@@ -133,14 +149,49 @@ $lista = $objeto->darListaEstudianteActivos();
 
 
 
+<div class="modal fade" id="modall" tabindex="-1" role="dialog" aria-labelledby="exampleModalFormTitle" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalFormTitle">Contactar</h5>
+
+            </div>
+            <div class="modal-body">
+                <form method="POST" id='formAgregarOf'>
+                    <div class="form-group">
+                    <select autocomplete="false" type="text" class="form-control" data-mask="999999999-9" placeholder="" aria-label="Username">
+                    <option disabled="" selected="">Seleccione el titulo de la promoción</option>
+                    <?php
+                    foreach ($for as $key) { 
+                    echo ('<option value="">'.$key[13].'</option>');
+                    }
+                    ?>
+                
+                
+                </select>
+                    </div>
+
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger btn-pill" data-dismiss="modal">Cerar</button>
+                <button type="button" class="btn btn-danger btn-pill" data-dismiss="modal">Enviar</button>
+
+            </div>
+        </div>
+    </div>
+</div>
 
 
 
-
-
+<script>
+    function mostrarModal1(valor) {
+        $('#modall').modal('show'); 
+    }
+</script>
 
 
 
 <?php
-    include('Footer.php')
-    ?>
+include('Footer.php')
+?>
