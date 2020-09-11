63c83a6f6c6c2e6ddba206dd925ebc2460e7bb30
gamendez <70824177+gamendez@users.noreply.github.com>
2020-09-11 05:23:59 -0500

    plantilla

---
+++ plantilla.php
@@ -0,0 +1,310 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="">
+<!--<![endif]-->
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Newline Template</title>
+  <!--
+Newline Template
+https://templatemo.com/tm-503-newline
+-->
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+  <link rel="stylesheet" href="assets/css/bootstrap.min2.css">
+  <!--  <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">-->
+  <link rel="stylesheet" href="assets/css/fontAwesome.css">
+  <link rel="stylesheet" href="assets/css/templatemo-style.css">
+
+
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+  <script src="assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+</head>
+
+<body>
+
+  <div class="overlay"></div>
+  <section class="top-part">
+    <video controls autoplay loop>
+      <source src="assets/videos/video.mp4" type="video/mp4">
+      <source src="assets/videos/video.ogg" type="video/ogg">
+      Your browser does not support the video tag.
+    </video>
+  </section>
+
+  <section class="cd-hero">
+
+    <div class="cd-slider-nav">
+      <nav>
+        <span class="cd-marker item-1"></span>
+        <ul>
+          <li class="selected"><a href="#0">
+              <div class="image-icon"><img src="assets/img/home-icon.png"></div>
+              <h6>Inicio</h6>
+            </a></li>
+          <li><a href="#0">
+              <div class="image-icon"><img src="assets/img/about-icon.png"></div>
+              <h6>Informacion</h6>
+            </a></li>
+          <li><a href="#0">
+              <div class="image-icon"><img src="assets/img/featured-icon.png"></div>
+              <h6>Requisitos</h6>
+            </a></li>
+          <li><a href="#0">
+              <div class="image-icon"><img src="assets/img/featured-icon.png"></div>
+              <h6>Estudiante</h6>
+            </a></li>
+          <li><a href="#0">
+              <div class="image-icon"><img src="assets/img/featured-icon.png"></div>
+              <h6>Empresa</h6>
+            </a></li>
+          <li><a href="#0">
+              <div class="image-icon"><img src="assets/img/contact-icon.png"></div>
+              <h6>Ingresa</h6>
+            </a></li>
+        </ul>
+      </nav>
+    </div> <!-- .cd-slider-nav -->
+
+    <ul class="cd-hero-slider">
+
+      <li class="selected">
+        <div class="heading">
+          <h1>Universidad El Bosque</h1>
+          <span>Bienvenido a la feria de oportunidades</span>
+        </div>
+        <div class="cd-full-width first-slide">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="content first-content">
+                  <h4>Sobre la feria</h4>
+                  <p>La XX Feria de Oportunidades Laborales de la Facultad de Ingeniería está dirigida a todos sus estudiantes próximos a realizar prácticas profesionales y sus egresados; para brindarles oportunidades laborales por parte de diferentes empresas que presentarán en dicho evento sus necesidades y perfiles requeridos.
+                    La Feria se desarrolla desde las 2:00 p. m. hasta las 7:00 p. m. y es de entrada libre.</p>
+                  <div class="primary-button">
+                    <a href="https://unbosque.edu.co/centro-informacion/evento/xx-feria-de-oportunidades-laborales">Saber mas</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+
+      <li>
+        <div class="heading">
+          <h1>Informacion</h1>
+          <span>
+            ¿Como participar?
+          </span>
+        </div>
+        <div class="cd-half-width second-slide">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="content second-content">
+                  <div class="row">
+                    <div class="col-md-7 left-image">
+                      <img src="assets/img/imagen1.jpg" width="700" height="300">
+                    </div>
+                    <div class="col-md-5">
+                      <div class="right-about-text">
+                        <h4>Para que sirve?</h4>
+                        <p>La feria de oportunidades pretende fortalecer la relacion Universidad-Empresa por lo que queremos
+                          mediante este evento, buscamos ser el puente para ue nuestros estudiantes logren contactar con la empresa
+                          y de igual forma la empresa con nuestros estudiantes.</p>
+                        <div class="primary-button">
+                          <!-- <a href="#">Discover More</a> -->
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-7">
+                      <div class="left-about-text">
+                        <h4>¿Como puedo participar?</h4>
+                        <p>Estudiante</br>
+                          Si eres un estudiante y estas interesado en participar, solo tienes que registrarte llenar tus datos y podras acceder a nuestra app.
+                        </p>
+                        <p>Empresa</br>
+                          Si eres una empresa y te interesa participar, debes ir al apartado de registro llenarlo con tus datos y ya esta!, podras acceder a nuestra app.
+                        </p>
+                        <div class="primary-button">
+                          <!-- <a href="#">Discover More</a> -->
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-5 right-image">
+                      <img src="assets/img/imagen2.jpg">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+
+      <li>
+        <div class="heading">
+          <h1>Requisitos</h1>
+          <span>Que debo saber?</span>
+        </div>
+        <div class="cd-half-width third-slide">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="content third-content">
+                  <div class="row">
+                    <div class="col-md-7 left-image">
+                      <img src="assets/img/imagen3.jpg">
+                    </div>
+                    <div class="col-md-5">
+                      <div class="right-feature-text">
+                        <h4> <em>Requisitos</em></h4>
+                        <p>Si quieres participar en nuestra feria debes tener en cuenta algunas reglas</p>
+                        <div class="feature-list">
+                          <ul>
+                            <p>Estudiante</br>
+                              -Debes registrarte con tu correo institucional.</br>
+
+                            </p>
+                            <p> Empresa</br>
+                              -Debes cargar tu camara de comercio en formato PDF.
+                            </p>
+
+                          </ul>
+                        </div>
+                        <div class="primary-button">
+                          <!-- <a href="#">Discover More</a> -->
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+
+      <li>
+        <div class="heading">
+          <h1>Registrate Estudiante</h1>
+          <span>Completa tu informacion</span>
+        </div>
+        <div class="cd-half-width fourth-slide">
+          <div class="container">
+
+            
+
+              <?php include_once 'registro1.php'; ?>
+
+
+
+           
+          </div>
+        </div>
+      </li>
+
+      <li>
+        <div class="heading">
+          <h1>Registrate Empresa</h1>
+          <span>Completa tu informacion</span>
+        </div>
+        <div class="cd-half-width fourth-slide">
+          <div class="container">
+
+            <div class="row">
+
+              <div class="col-md-12">
+                <div class="content fourth-content">
+
+
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+
+      <li>
+        <div class="heading">
+          <h1>Login</h1>
+          <span>Informacion de usuario</span>
+        </div>
+        <div class="cd-half-width fivth-slide">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="content fivth-content">
+                  <div class="row">
+                    <div class="col-md-4">
+                      <div class="left-info">
+                        <p>Empresa y Universidad fortaleciendo la responsabilidad social a traves de la practica profesional.
+                          <br><br>
+                          <em>Línea gratuita:<br>
+                            018000 113033<br>
+                            Edificio Fundadores<br>
+                            Av. Cra. 9 No. 131 A - 02<br></em>
+                        </p>
+                        <ul class="social-icons">
+                          <i><a href="#"><i class="fa fa-facebook"></i></a></i>
+                          <i><a href="#"><i class="fa fa-twitter"></i></a></i>
+                          <i><a href="#"><i class="fa fa-linkedin"></i></a></i>
+                          <i><a href="#"><i class="fa fa-rss"></i></a></i>
+                          <i><a href="#"><i class="fa fa-behance"></i></a></i>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="col-md-8">
+                      <div clas="row justify-content-md-center">
+
+                        <div class="col-md-5"></div>
+                        <form method="POST" id="login">
+
+                          <input name="name" type="email" class="form-control" id="correo" placeholder="Correo universitario" required="">
+                          <input name="email" type="password" class="form-control" id="contra" placeholder="contraseña" required="">
+                        </form>
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </li>
+    </ul> <!-- .cd-hero-slider -->
+  </section> <!-- .cd-hero -->
+
+
+  <footer>
+    <p>Copyright &copy; 2020 Your Company
+
+      | Designed by <em>Template Mo</em></p>
+  </footer>
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <script>
+    window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')
+  </script>
+
+  <script src="assets/js/vendor/bootstrap.min.js"></script>
+  <script src="assets/js/plugins.js"></script>
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ registro1.php
@@ -0,0 +1,23 @@
+<div class="container d-flex flex-column justify-content-between vh-30">
+        <div class="row justify-content-center mt-5">
+            <div class="col-lg-8 col-sm-offset-1">
+                <div class="card card-default">
+                    <div class="card-header card-header  d-flex justify-content-center">
+                        <img src="assets/img/logo.png" style="width:150px ;" alt="">
+                    </div>
+                    <div class="card-body">
+                        
+                        <div class="tab-content" id="myTabContent4">
+                           
+                            <div class="tab-pane pt-3 " id="profile3" role="tabpanel"
+                                aria-labelledby="profile3-tab">
+                                <?php include_once 'registro_estudiante.php'; ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
