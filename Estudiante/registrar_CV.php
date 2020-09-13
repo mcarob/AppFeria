d5b5e4505359a07a31c9ca3f06886c7aee0fdb7d
Sebastias1100 <70824246+Sebastias1100@users.noreply.github.com>
2020-09-13 13:46:56 -0500

    VERSION_HV_1.0

--- Estudiante/registrar_CV.php
+++ Estudiante/registrar_CV.php
@@ -4,95 +4,136 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/ProyectoFeria/AppFeria/Controlador/Cont
 include_once($_SERVER['DOCUMENT_ROOT'].'/ProyectoFeria/AppFeria/Modelo/Entidades/HojaDeVida.php');
 
 
-//RECOLECCION DE VARIAS FORMACIONES ACADMICAS, (FORMULARIO FORMACIONES) 
+
+//--------------INICIO FORMULARIO DATOS PERSONALES------------------------//
+
+//RECOLECCION DE DATOS DE PERSONALES (FORMULARIO DATOS PERSONALES)
+$datosPersonales=array(
+
+    $_POST["nombre"],
+    $_POST["apellidos"],
+    $_POST["cedula"],
+    $_POST["telefono"],
+    $_POST["ciudad"],
+    $_POST["direccion"],
+    $_POST["correo"],
+    
+    $_POST["nombreR1"],
+    $_POST["cargoR1"],
+    $_POST["telefonoR1"],
+
+    $_POST["nombreR2"],
+    $_POST["cargoR2"],
+    $_POST["telefonoR2"]
+    
+    
+);
+
+//--------------INICIO FORMULARIO FORMACIONES-----------------------------//
+
+//RECOLECCION DE VARIAS FORMACIONES ACADEMICAS, (FORMULARIO FORMACIONES) 
 $formacionAcademica=array(
     $_POST["academica"]
    );
-$institucion=array(
-   $_POST["finAca"]);
+$instituciones=array(
+   $_POST["institucion"]);
 
-$InicioAca=array(
+$IniciosAca=array(
     $_POST["inicioAca"] 
 );  
 
-$FinAca=array(
+$FinesAca=array(
     $_POST["finAca"]
 );
+
 //RECOLECCION DE VARIAS FORMACIONES COMPLEMENTARIAS, (FORMULARIO FORMACIONES) 
 $formacionComplementaria=array(
     $_POST["complementaria"]
-   );
-$institucion=array(
-   $_POST["finAca"]);
+);
+$cursos=array(
+   $_POST["nombreCurso"]);
 
-$InicioAca=array(
-    $_POST["inicioAca"] 
+$iniciosCom=array(
+    $_POST["inicioComp"] 
 );  
 
-$FinAca=array(
-    $_POST["finAca"]
+$finesCom=array(
+    $_POST["finComp"]
 );
 
-//RECOLECCION DE DATOS DE FORMACIONES
-// $datosFormacion=array(
+//RECOLECCION DE DATOS DE FORMACIONES (FORMULARIO FORMACIONES)
+$datosFormacion=array(
+//---datos formacion academica    
 
-//DATOS FORMACION ACADEMICA    
+$_POST["perfil"],
+$formacionAcademica,
+$instituciones,
+$IniciosAca,
+$FinesAca,
 
-//  $_POST["perfil"],
-// $formacionAcademica,
-// $institucion,
-// $InicioAca,
-// $FinAca,
+//---datos formacion complementaria
+
+$formacionComplementaria,
+$cursos,
+$iniciosCom,
+$finesCom
+);
+
+//----------------INICIO FORMULARIO EXPERIENCIAS----------------//
+
+//Recoleccion de varias experiencias academicas
+$proyectos=array(
+    $_POST["proyecto"]
+);
+$materias=array(
+    $_POST["materia"]
+);
+$periodos=array(
+    $_POST["periodo"]
+);
+$descProyectos=array(
+    $_POST["periodo"]
+);
+
+//Recoleccion de varias experiencias profesionales
+$cargos=array(
+    $_POST["cargoPro"]
+);
+$empresas=array(
+    $_POST["empresa"]
+);
+$iniciosLab=array(
+    $_POST["inicioLab"]
+);
+$finesLab=array(
+    $_POST["finLab"]
+);
+$descCargos=array(
+    $_POST["desCargo"]
+);
+
+$datosExperiencia=array(
+//----Datos experiencia academica    
+    $proyectos,
+    $materias,
+    $periodos,
+    $descProyectos,
+
+//---Datos experiencia profesional
+    $cargos,
+    $empresas,
+    $iniciosLab,
+    $finesLab,
+    $descCargos
+);
+echo("---------------PERSONAL-----------");
+print_r($datosPersonales);
+
+echo("-----------FORMACION-----------------");
+print_r($datosFormacion);
 
 
-//DATOS FORMACION COMPLEMENTARIA
-
-// $_POST["complementaria"],
-// $_POST["nombreCurso"],
-// $_POST["nombreCurso"],
-// $_POST["inicioComp"],
-// $_POST["finComp"],
-// );
-
-
-
-// $datosPersonales=array(
-//     $_POST["nombre"],
-//     $_POST["apellidos"],
-//     $_POST["cedula"],
-//     $_POST["telefono"],
-//     $_POST["ciudad"],
-//     $_POST["direccion"],
-//     $_POST["correo"],
-    
-//     $_POST["nombreR1"],
-//     $_POST["cargoR1"],
-//     $_POST["telefonoR1"],
-
-//     $_POST["nombreR2"],
-//     $_POST["cargoR2"],
-//     $_POST["telefonoR2"]
-    
-    
-// );
-
-// $datosExperiencia=array(
-//     $_POST["proyecto"],
-//     $_POST["materia"],
-//     $_POST["periodo"],
-//     $_POST["descProy"],
-
-//     $_POST["cargoPro"],
-//     $_POST["empresa"],
-//     $_POST["inicioLab"],
-//     $_POST["finLab"],
-//     $_POST["desCargo"]
-// );
-// echo("EXPERIENCIA");
-// print_r($datosExperiencia);
-// echo("PERSONAL");
-// print_r($datosPersonales);
-// echo("FORMACION");
-// print_r($datosFormacion);
+echo("-------------EXPERIENCIA-------------");
+print_r($datosExperiencia);
 
 ?>
