d7ecb4a17c6eae97c0424d470a359e73b9453745
mcarob <37860253+mcarob@users.noreply.github.com>
2020-09-13 15:56:16 -0500

    Update EnviarCorreos.php

--- Controlador/EnviarCorreos.php
+++ Controlador/EnviarCorreos.php
@@ -12,39 +12,44 @@ class enviarCorreo{
 
 
     public function enviarMensaje($nombre,$para,$asunto,$mensaje){
-        $mail = new PHPMailer(true);
-        $mail->isSMTP();
-        $mail->Host='smtp.gmail.com';
-        $mail->Port=587;
-        $mail->SMTPAuth=true;
-        $mail->Username='proyectoferia20202@gmail.com';
-        $mail->Password='feria1234';
-        $mail->CharSet = 'UTF-8';
-        $ruta=$_SERVER['DOCUMENT_ROOT'] . '/ProyectoFeria/AppFeria/assets/img/logo1.png';
-        $mail->setFrom('proyectoferia20202@gmail.com','Feria Laboral Universidad El Bosque');
-        $mail->addAddress($para);
-        $mail->subject=$asunto;
-        $mail->isHTML(true);
-        $mail->AddEmbeddedImage($ruta, 'my-photo', 'logo.png'); 
-        $mail->Body='
-                    <meta http-equiv=”Content-Type” content=”text/html; charset=UTF-8″ />
-                    <div style="display:flex;
-                    align-items:center;
-                    justify-content:center;">  <img  alt="PHPMailer" src="cid:my-photo">    
-                    </div>
-
-                    <h1 align=center> Feria de Oportunidades Universidad El Bosque </h1>
-                    <br> 
-                    <br>
-                    <p> Muy buenas '.$nombre.'  </p>
-                    <br>
-                    <p>'.$mensaje.'  </p>
-                    ';
-if(!$mail->send()){
-    return 0;
-}else{
-    return 1;
-}
+        try {
+            $mail = new PHPMailer(true);
+            $mail->isSMTP();
+            $mail->Host='smtp.gmail.com';
+            $mail->Port=587;
+            $mail->SMTPAuth=true;
+            $mail->Username='proyectoferia20202@gmail.com';
+            $mail->Password='feria1234';
+            $mail->CharSet = 'UTF-8';
+            $ruta=$_SERVER['DOCUMENT_ROOT'] . '/ProyectoFeria/AppFeria/assets/img/logo1.png';
+            $mail->setFrom('proyectoferia20202@gmail.com','Feria Laboral Universidad El Bosque');
+            $mail->addAddress($para);
+            $mail->subject=$asunto;
+            $mail->isHTML(true);
+            $mail->AddEmbeddedImage($ruta, 'my-photo', 'logo.png'); 
+            $mail->Body='
+                        <meta http-equiv=”Content-Type” content=”text/html; charset=UTF-8″ />
+                        <div style="display:flex;
+                        align-items:center;
+                        justify-content:center;">  <img  alt="PHPMailer" src="cid:my-photo">    
+                        </div>
+    
+                        <h1 align=center> Feria de Oportunidades Universidad El Bosque </h1>
+                        <br> 
+                        <br>
+                        <p> Muy buenas '.$nombre.'  </p>
+                        <br>
+                        <p>'.$mensaje.'  </p>
+                        ';
+    if(!$mail->send()){
+        return 0;
+    }else{
+        return 1;
+    }
+          }  catch (Exception $e) {
+            echo $e->getMessage(); //Boring error messages from anything else!
+          }
+        
 
     }
     public function enviarMensajeF($nombre,$para,$asunto,$mensaje){
