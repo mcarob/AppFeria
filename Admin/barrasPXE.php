633b18a126562e1de455a55184e60a292a5100a2
acubilloso <acubilloso@unbosque.edu.co>
2020-09-13 18:06:11 -0500

    Update barrasPXE.php

--- Admin/barrasPXE.php
+++ Admin/barrasPXE.php
@@ -39,20 +39,25 @@ $datosY = json_encode($valoresY);
 	var data = [{
 		x: datosX,
 		y: datosY,
+		textposition: 'auto',
 		type: 'bar',
 		marker: {
-			color: 'blue'
+			color: 'rgb(158,202,225)',
+			opacity: 0.6,
+			line: {
+				color: 'rgb(8,48,107)',
+				width: 1.5
+			}
+
 		}
 	}];
-	
+
 
 	var layout = {
 		font: {
 			family: 'Raleway, sans-serif'
 		},
-		xaxis: {
-			tickangle: -30,
-		},
+	
 		bargap: 0.05
 	};
 
