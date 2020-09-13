850f433fcf730197549eb0f2f54cfbe202ea7dc6
Sebastias1100 <70824246+Sebastias1100@users.noreply.github.com>
2020-09-13 12:24:02 -0500

    Update ExperienciasCV.php

--- Estudiante/ExperienciasCV.php
+++ Estudiante/ExperienciasCV.php
@@ -20,8 +20,8 @@
                                 <i class="material-icons">assignment_ind</i>
                             </span>
                         </div>
-                        <input autocomplete="false" type="text" class="form-control" data-mask="999999999-9"
-                            placeholder="Titulo del proyecto" aria-label="Username" id="proyecto" name="proyecto">
+                        <input autocomplete="false" type="text" class="form-control" 
+                            placeholder="Titulo del proyecto" aria-label="Username" id="proyecto[]" name="proyecto[]">
                     </div>
                 </div>
                 <div class="col-md-6 mb-3">
@@ -32,7 +32,7 @@
                             </span>
                         </div>
                         <input autocomplete="false" type="text" class="form-control" placeholder="Materia"
-                            aria-label="Username" id="materia" name="materia">
+                            aria-label="Username" id="materia[]" name="materia[]">
                     </div>
                 </div>
                 <div class="col-md-6 mb-3">
@@ -42,8 +42,8 @@
                                 <i class="material-icons">assignment_ind</i>
                             </span>
                         </div>
-                        <select autocomplete="false" type="text" class="form-control" data-mask="999999999-9"
-                            aria-label="Username" id="periodo" name="periodo">
+                        <select autocomplete="false" type="text" class="form-control" 
+                            aria-label="Username" id="periodo[]" name="periodo[]">
                             <option disabled="" selected="">Seleccionar periodo</option>
                             <option value="bg">Periodo 2015-1 </option>
                             <option value="md">Periodo 2015-2 </option>
@@ -64,7 +64,7 @@
 
             </div>
             <div class="form-group">
-                <textarea class="form-control" id="descProy" name="descProy" rows="5" style="resize: none;"
+                <textarea class="form-control" id="descProy[]" name="descProy[]" rows="5" style="resize: none;"
                     placeholder="Describa del proyecto"></textarea>
             </div>
         </div>
@@ -93,8 +93,8 @@
                                 <i class="material-icons">assignment_ind</i>
                             </span>
                         </div>
-                        <input autocomplete="false" type="text" class="form-control" data-mask="999999999-9"
-                            placeholder="Cargo" aria-label="Username" id="cargoPro" name="cargoPro">
+                        <input autocomplete="false" type="text" class="form-control" 
+                            placeholder="Cargo" aria-label="Username" id="cargoPro[]" name="cargoPro[]">
                     </div>
                 </div>
                 <div class="col-md-6 mb-3">
@@ -105,7 +105,7 @@
                             </span>
                         </div>
                         <input autocomplete="false" type="text" class="form-control" placeholder="Empresa"
-                            aria-label="Username" id="empresa" name="empresa">
+                            aria-label="Username" id="empresa[]" name="empresa[]">
                     </div>
                 </div>
                 <div class="col-md-6 mb-3">
@@ -118,7 +118,7 @@
                             </span>
                         </div>
                         <input autocomplete="false" type="date" class="form-control" placeholder=""
-                            aria-label="Username" id="inicioLab" name="inicioLab">
+                            aria-label="Username" id="inicioLab[]" name="inicioLab[]">
                     </div>
                 </div>
                 <div class="col-md-6 mb-3">
@@ -131,14 +131,14 @@
                             </span>
                         </div>
                         <input autocomplete="false" type="date" class="form-control" placeholder=""
-                            aria-label="Username" id="finLab" name="finLab">
+                            aria-label="Username" id="finLab[]" name="finLab[]">
                     </div>
                 </div>
 
             </div>
             <div class="form-group">
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="5" style="resize: none;"
-                    placeholder="Describa el cargo" id="desCargo" name="desCargo"></textarea>
+                <textarea class="form-control" rows="5" style="resize: none;"
+                    placeholder="Describa el cargo" id="desCargo[]" name="desCargo[]"></textarea>
             </div>
 
         </div>
@@ -152,7 +152,7 @@ $(document).ready(function() {
     $('#add').click(function() {
         i++;
         $('#agregar').append('<div id="row' + i +
-            '" class="sumaAca"> <div class="buttons"> </div> <br> <div class="elementAca"> <div class="row no-gutters"> <div class="col-md-6 mb-3"> <div class="input-group"> <div class="input-group-prepend"> <span class="input-group-text"> <i class="material-icons">assignment_ind</i> </span> </div> <input autocomplete="false" type="text" class="form-control" data-mask="999999999-9" placeholder="Titulo del proyecto" aria-label="Username" id="proyecto" name="proyecto"> </div> </div> <div class="col-md-6 mb-3"> <div class="input-group"> <div class="input-group-prepend"> <span class="input-group-text"> <i class="material-icons">phone</i> </span> </div> <input autocomplete="false" type="text" class="form-control" placeholder="Materia" aria-label="Username" id="materia" name="materia"> </div> </div> <div class="col-md-6 mb-3"> <div class="input-group"> <div class="input-group-prepend"> <span class="input-group-text"> <i class="material-icons">assignment_ind</i> </span> </div> <select autocomplete="false" type="text" class="form-control" data-mask="999999999-9" aria-label="Username" id="periodo" name="periodo"> <option disabled="" selected="">Seleccionar periodo</option> <option value="bg">Periodo 2015-1 </option> <option value="md">Periodo 2015-2 </option> <option value="cl">Periodo 2016-1 </option> <option value="cl">Periodo 2016-2 </option> <option value="cl">Periodo 2017-1 </option> <option value="cl">Periodo 2017-2 </option> <option value="cl">Periodo 2018-1 </option> <option value="cl">Periodo 2018-2 </option> <option value="cl">Periodo 2019-1 </option> <option value="cl">Periodo 2019-2 </option> <option value="cl">Periodo 2020-1 </option> <option value="cl">Periodo 2020-2 </option> </select> </div> </div> </div> <div class="form-group"> <textarea class="form-control" id="descProy" name="descProy" rows="5" style="resize: none;" placeholder="Describa del proyecto"></textarea> </div> </div><button type="button" name="remove" id="' +
+            '" class="sumaAca"> <div class="buttons"> </div> <br> <div class="elementAca"> <div class="row no-gutters"> <div class="col-md-6 mb-3"> <div class="input-group"> <div class="input-group-prepend"> <span class="input-group-text"> <i class="material-icons">assignment_ind</i> </span> </div> <input autocomplete="false" type="text" class="form-control" placeholder="Titulo del proyecto" aria-label="Username" id="proyecto[]" name="proyecto[]"> </div> </div> <div class="col-md-6 mb-3"> <div class="input-group"> <div class="input-group-prepend"> <span class="input-group-text"> <i class="material-icons">phone</i> </span> </div> <input autocomplete="false" type="text" class="form-control" placeholder="Materia" aria-label="Username" id="materia[]" name="materia[]"> </div> </div> <div class="col-md-6 mb-3"> <div class="input-group"> <div class="input-group-prepend"> <span class="input-group-text"> <i class="material-icons">assignment_ind</i> </span> </div> <select autocomplete="false" type="text" class="form-control" aria-label="Username" id="periodo[]" name="periodo[]"> <option disabled="" selected="">Seleccionar periodo</option> <option value="bg">Periodo 2015-1 </option> <option value="md">Periodo 2015-2 </option> <option value="cl">Periodo 2016-1 </option> <option value="cl">Periodo 2016-2 </option> <option value="cl">Periodo 2017-1 </option> <option value="cl">Periodo 2017-2 </option> <option value="cl">Periodo 2018-1 </option> <option value="cl">Periodo 2018-2 </option> <option value="cl">Periodo 2019-1 </option> <option value="cl">Periodo 2019-2 </option> <option value="cl">Periodo 2020-1 </option> <option value="cl">Periodo 2020-2 </option> </select> </div> </div> </div> <div class="form-group"> <textarea class="form-control" id="descProy[]" name="descProy[]" rows="5" style="resize: none;" placeholder="Describa del proyecto"></textarea> </div> </div><button type="button" name="remove" id="' +
             i + '" class="btn btn-danger btn_remove">Remover</button>');
 
     });
@@ -160,7 +160,7 @@ $(document).ready(function() {
     $('#add2').click(function() {
         i++;
         $('#agregar2').append('<div id="row' + i +
-            '" class="elementPro"> <div class="row no-gutters"> <div class="col-md-6 mb-3"> <div class="input-group"> <div class="input-group-prepend"> <span class="input-group-text"> <i class="material-icons">assignment_ind</i> </span> </div> <input autocomplete="false" type="text" class="form-control" data-mask="999999999-9" placeholder="Cargo" aria-label="Username" id="cargoPro" name="cargoPro"> </div> </div> <div class="col-md-6 mb-3"> <div class="input-group"> <div class="input-group-prepend"> <span class="input-group-text"> <i class="material-icons">phone</i> </span> </div> <input autocomplete="false" type="text" class="form-control" placeholder="Empresa" aria-label="Username" id="empresa" name="empresa"> </div> </div> <div class="col-md-6 mb-3"> <h5>Fecha de inicio</h5> <br> <div class="input-group"> <div class="input-group-prepend"> <span class="input-group-text"> <i class="material-icons">assignment_ind</i> </span> </div> <input autocomplete="false" type="date" class="form-control" placeholder="" aria-label="Username" id="inicioLab" name="inicioLab"> </div> </div> <div class="col-md-6 mb-3"> <h5>Fecha de finalizacion</h5> <br> <div class="input-group"> <div class="input-group-prepend"> <span class="input-group-text"> <i class="material-icons">assignment_ind</i> </span> </div> <input autocomplete="false" type="date" class="form-control" placeholder="" aria-label="Username" id="finLab" name="finLab"> </div> </div> </div> <div class="form-group"> <textarea class="form-control" id="exampleFormControlTextarea1" rows="5" style="resize: none;" placeholder="Describa el cargo" id="desCargo" name="desCargo"></textarea> </div><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">Remover</button><br><br>');
+            '" class="elementPro"> <div class="row no-gutters"> <div class="col-md-6 mb-3"> <div class="input-group"> <div class="input-group-prepend"> <span class="input-group-text"> <i class="material-icons">assignment_ind</i> </span> </div> <input autocomplete="false" type="text" class="form-control" placeholder="Cargo" aria-label="Username" id="cargoPro[]" name="cargoPro[]"> </div> </div> <div class="col-md-6 mb-3"> <div class="input-group"> <div class="input-group-prepend"> <span class="input-group-text"> <i class="material-icons">phone</i> </span> </div> <input autocomplete="false" type="text" class="form-control" placeholder="Empresa" aria-label="Username" id="empresa[]" name="empresa[]"> </div> </div> <div class="col-md-6 mb-3"> <h5>Fecha de inicio</h5> <br> <div class="input-group"> <div class="input-group-prepend"> <span class="input-group-text"> <i class="material-icons">assignment_ind</i> </span> </div> <input autocomplete="false" type="date" class="form-control" placeholder="" aria-label="Username" id="inicioLab[]" name="inicioLab[]"> </div> </div> <div class="col-md-6 mb-3"> <h5>Fecha de finalizacion</h5> <br> <div class="input-group"> <div class="input-group-prepend"> <span class="input-group-text"> <i class="material-icons">assignment_ind</i> </span> </div> <input autocomplete="false" type="date" class="form-control" placeholder="" aria-label="Username" id="finLab[]" name="finLab[]"> </div> </div> </div> <div class="form-group"> <textarea class="form-control" rows="5" style="resize: none;" placeholder="Describa el cargo" id="desCargo[]" name="desCargo[]"></textarea> </div><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">Remover</button><br><br>');
 
     });
 
