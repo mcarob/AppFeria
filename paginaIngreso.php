28d6dda06f1e4c39ce3826572d2e0ae057946f87
mcarob <37860253+mcarob@users.noreply.github.com>
2020-09-14 14:55:45 -0500

    controlador user pagina ingreso

--- paginaIngreso.php
+++ paginaIngreso.php
@@ -4,7 +4,8 @@ include_once('Controlador/user.php');
 
 $userSession = new UserSession();
 $user = new Usuario();
-
+$userForm = "";
+	$passForm = "";
 if(isset($_SESSION['user'])){
 	$user->setUser($userSession->getCurrentUser());
 	$tipo=$user->getTipoUsuario();
@@ -20,17 +21,50 @@ if(isset($_SESSION['user'])){
     $userForm = $_POST['username'];
 	$passForm = $_POST['password'];
 	if($user->darIngreso($userForm,$passForm)){
-        $userSession->setCurrentUser($userForm);
-		$user->setUser($userForm);
-        $tipo=$user->getTipoUsuario();
-        $userSession->setTipoUsuario($tipo);
-		if($tipo==1 || $tipo==4){
-            header('location: Admin/index.php');
-            }else if($tipo==2){
-            header('location: Estudiante/index.php');
-            }else if($tipo==3){
-                header('location: Empresa/index.php');
-            }
+		if($user->darEstado()==1){
+			$userSession->setCurrentUser($userForm);
+			$user->setUser($userForm);
+			$tipo=$user->getTipoUsuario();
+			$userSession->setTipoUsuario($tipo);
+			if($tipo==1 || $tipo==4){
+				header('location: Admin/index.php');
+				}else if($tipo==2){
+				header('location: Estudiante/index.php');
+				}else if($tipo==3){
+					header('location: Empresa/index.php');
+				}
+		}elseif($user->darEstado()==2){
+			if(isset($_POST['verifi'])){
+				if(md5(($_POST['verifi']))==$user->darVerificacion()){
+					$userSession->setCurrentUser($userForm);
+					$user->setUser($userForm);
+					$tipo=$user->getTipoUsuario();
+					$userSession->setTipoUsuario($tipo);
+					$user->cambiarEstadoValido();
+					if($tipo==1 || $tipo==4){
+						header('location: Admin/index.php');
+						}else if($tipo==2){
+						header('location: Estudiante/index.php');
+						}else if($tipo==3){
+						header('location: Empresa/index.php');
+						}
+				}else{
+					$mostrarCodigo=true;
+					$errorEntrada="El codigo de verificación es incorrecto";
+					include_once 'ingresoF.php';
+				}
+				
+			}else{
+				$mostrarCodigo=true;
+				include_once 'ingresoF.php';
+				echo("entraaaaaar 37");
+			}
+			
+		}else{
+			echo($user->darEstado());
+			echo("entro en 41");
+		}
+      
 
 	}else{
 		$errorEntrada="nombre de usuario o contraseña incorrecto";
@@ -47,3 +81,4 @@ if(isset($_SESSION['user'])){
 
 
 ?>
+
