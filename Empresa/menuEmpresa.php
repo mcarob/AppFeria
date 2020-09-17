2888a877e2e1910586c025abb49e8f3ac8d953a3
mcarob <37860253+mcarob@users.noreply.github.com>
2020-09-16 23:45:46 -0500

    Update menuEmpresa.php

--- Empresa/menuEmpresa.php
+++ Empresa/menuEmpresa.php
@@ -3,6 +3,7 @@
 
 <head>
   <meta charset="utf-8" />
+<meta http-equiv="Content-type" content="text/html; charset=UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta name="description" content="Sleek Dashboard - Free Bootstrap 4 Admin Dashboard Template and UI Kit. It is very powerful bootstrap admin dashboard, which allows you to build products like admin panels, content management systems and CRMs etc.">
@@ -36,7 +37,10 @@
 
 <body class="header-fixed sidebar-fixed sidebar-dark header-light" id="body">
 
+<?php 
+    $user = new Usuario();
 
+?>
 
     <!--
           ====================================
