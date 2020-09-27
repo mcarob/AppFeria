3aa09bb2cf41d1915f8ca3f4502572a01e683689
acubilloso <acubilloso@unbosque.edu.co>
2020-09-26 22:29:55 -0500

    Update legalizar.php

--- Empresa/legalizar.php
+++ Empresa/legalizar.php
@@ -2,15 +2,17 @@
 
 include_once($_SERVER['DOCUMENT_ROOT'] . '/ProyectoFeria/AppFeria/Controlador/ControladorPostulaciones.php');
 include_once($_SERVER['DOCUMENT_ROOT'] . '/ProyectoFeria/AppFeria/Modelo/Entidades/PromocionPostulacion.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/ProyectoFeria/AppFeria/Controlador/ControladorPromocion.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/ProyectoFeria/AppFeria/Modelo/Entidades/PromocionLaboral.php');
+
 $controlador = new ControladorPostulacion;
+$controladorL = new ControladorPromocion;
 
 if (isset($_GET["action"])) {
     $var = $_GET["action"];
     $promocion = new PromocionPostulacion(0, 13 , $var, 5, null, null, null);
     echo ($controlador->agregarPostulacion($promocion));
 }
-
-
 
 ?>
 
