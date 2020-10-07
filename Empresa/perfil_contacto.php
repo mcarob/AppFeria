a30dccd275fecbb9cf015ca8b00d238b26f17b80
Sebastias1100 <70824246+Sebastias1100@users.noreply.github.com>
2020-10-07 01:06:37 -0500

    ACT_EMPR

--- Empresa/perfil_contacto.php
+++ Empresa/perfil_contacto.php
@@ -1,6 +1,7 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'] . '/ProyectoFeria/AppFeria/Controlador/user.php');
 include_once($_SERVER['DOCUMENT_ROOT'] . '/ProyectoFeria/AppFeria/Modelo/Daos/ContactoEmpresaDAO.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/ProyectoFeria/AppFeria/Modelo/Daos/EmpresaDAO.php');
 include_once($_SERVER['DOCUMENT_ROOT'] . '/ProyectoFeria/AppFeria/Modelo/Entidades/ContactoEmpresa.php');
 session_start();
 if (!isset($_SESSION['user'])) {
@@ -17,6 +18,8 @@ $codigo = $user->darCodigo();
 $contactoEmpresa = $contactoEmpresa_dao->devolverContactoEmpresa($codigo);
 $nombreContacto = $contactoEmpresa_dao->devolverNombreContacto($codigo);
 
+$empresa_dao = new EmpresaDAO();
+$empresa = $empresa_dao->devolverEmpresa($codigo);
 
 ?>
 
@@ -37,12 +40,14 @@ include('Header.php');
                     <div class="col-lg-4 col-xl-3">
                         <div class="profile-content-left pt-5 pb-3 px-3 px-xl-5">
                             <div class="card text-center widget-profile px-0 border-0">
-                                <div class="card-img mx-auto rounded-circle">
-                                    <img src="../Imagenes/logo.png" width="90" alt="user image">
+                                <div class="card text-center widget-profile px-0 border-0">
+                                <?php   
+                                echo '<img style="max-width:100%;width:auto;height:auto;" src="data:image/jpeg;base64,'.base64_encode( $empresa->getLogoEmpresa() ).'" lt="user image"/>';
+                                ?>
                                 </div>
 
                                 
-                                    <h4 class="py-2 text-dark">Feria de opotunidades</h4>
+                                    <h4 class="py-2 text-dark"><?php echo($empresa->getRazonSocial()) ?></h4>
 
                             </div>
 
